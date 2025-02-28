e97793dcd227a376b068e28cff8391ac80d4d946
Jannik Fachat <149075215+minustimesplus@users.noreply.github.com>
2025-02-28 12:53:30 +0000

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-  }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  root: './', // Ensure root is set to the correct directory
+  publicDir: 'public',
 });
