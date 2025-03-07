9397afcab3fcf2c5b655d78823cff90829044d7e
Jannik Fachat <149075215+minustimesplus@users.noreply.github.com>
2025-03-07 19:44:56 +0000

    .

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
       <header className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2 flex justify-between items-center">
           <div className="flex items-center">
-            <img src={logo} alt="IB Paper Tracker Logo" className="h-20 w-auto mr-3" />
+            <img src={logo} alt="IB Paper Tracker Logo" className="h-10 w-auto mr-3" />
           </div>
           <div className="flex items-center space-x-4">
             {currentUser ? (
